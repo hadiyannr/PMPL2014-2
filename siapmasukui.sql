20497c95737e09e6b8eb802001fd7e410405c1b4
hanifnaufal <hanifnaufal7557@gmail.com>
2014-04-02 09:46:56 +0700

    update sql

--- siapmasukui.sql
+++ siapmasukui.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2014 at 04:31 AM
+-- Generation Time: Apr 02, 2014 at 04:41 AM
 -- Server version: 5.5.34
 -- PHP Version: 5.4.22
 
@@ -27,13 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `jawaban` (
-  `idPengerjaan` int(11) NOT NULL,
-  `idTryout` int(11) NOT NULL,
-  `idSoal` int(11) NOT NULL,
-  `isiJawaban` tinyint(1) DEFAULT NULL,
-  `isTrue` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`idPengerjaan`,`idTryout`,`idSoal`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idpengerjaan` int(11) NOT NULL,
+  `idsoal` int(11) NOT NULL,
+  `isiJawaban` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jawabaan_pengerjaan_idx` (`idpengerjaan`),
+  KEY `soal_jawab_idx` (`idsoal`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -85,17 +86,30 @@ INSERT INTO `konten` (`id`, `idcategory`, `isi`, `judul`, `isPublished`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `opsi` (
-  `idTryout` int(11) NOT NULL,
-  `idSoal` int(11) NOT NULL,
-  `idPengerjaan` int(11) NOT NULL,
-  `nomor` varchar(5) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idsoal` int(11) NOT NULL,
   `isJawaban` tinyint(1) DEFAULT NULL,
+  `nomor` varchar(10) NOT NULL,
   `pernyataan` text NOT NULL,
-  PRIMARY KEY (`idTryout`,`idSoal`,`idPengerjaan`),
-  KEY `idTryout_idx` (`idTryout`),
-  KEY `soal_opsi_idx` (`idTryout`,`idSoal`),
-  KEY `pengerjaan_opsi_idx` (`idPengerjaan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `soal_opsi_idx` (`idsoal`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+
+--
+-- Dumping data for table `opsi`
+--
+
+INSERT INTO `opsi` (`id`, `idsoal`, `isJawaban`, `nomor`, `pernyataan`) VALUES
+(35, 21, 0, '0', 'a'),
+(36, 21, 0, '1', 'b'),
+(37, 21, 0, '2', 'c'),
+(38, 21, 0, '3', 'd'),
+(39, 21, 0, '4', 'e'),
+(40, 1, 0, '0', 'lala1'),
+(41, 1, 0, '1', 'lala2'),
+(42, 1, 0, '2', 'lala3'),
+(43, 1, 0, '3', 'lala4'),
+(44, 1, 0, '4', 'lala51');
 
 -- --------------------------------------------------------
 
@@ -125,8 +139,18 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `isAdmin` tinyint(1) NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `username`, `email`, `password`, `isAdmin`, `isActive`) VALUES
+(1, 'hanif', 'hanif@hanif.com', 'dingdong', 1, 1),
+(2, 'user', 'user@user.com', 'user', 0, 0),
+(4, 'hanifnaufal', 'hanif@naufal.com', '0d5ae719d9c263ff078793a4f0bf25fd', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -135,13 +159,15 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
 --
 
 CREATE TABLE IF NOT EXISTS `profil` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) DEFAULT NULL,
   `fotoUrl` varchar(255) DEFAULT NULL,
-  `jenisKelamin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`,`nama`),
-  KEY `pengguna_id_idx` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `jenisKelamin` tinyint(1) DEFAULT NULL,
+  `idpengguna` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pengguna_id_idx` (`id`),
+  KEY `pengguna_id_idx1` (`idpengguna`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -150,11 +176,25 @@ CREATE TABLE IF NOT EXISTS `profil` (
 --
 
 CREATE TABLE IF NOT EXISTS `soal` (
-  `idTryout` int(11) NOT NULL,
-  `pertanyaan` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idtryout` int(11) NOT NULL,
+  `pertanyaan` text NOT NULL,
   `nomor` int(11) NOT NULL,
-  PRIMARY KEY (`idTryout`,`nomor`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idtryout` (`idtryout`,`nomor`),
+  UNIQUE KEY `idtryout_2` (`idtryout`,`nomor`),
+  KEY `tryout_soal_idx` (`idtryout`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+--
+-- Dumping data for table `soal`
+--
+
+INSERT INTO `soal` (`id`, `idtryout`, `pertanyaan`, `nomor`) VALUES
+(1, 1, 'ini soal tapi udah diganti\r\n', 1),
+(2, 2, 'Ini soal to 2 no 1\r\nudah diedit\r\n', 1),
+(21, 1, 'Ini soal dingdong', 2),
+(22, 1, 'qwertyui', 3);
 
 -- --------------------------------------------------------
 
@@ -164,14 +204,32 @@ CREATE TABLE IF NOT EXISTS `soal` (
 
 CREATE TABLE IF NOT EXISTS `tryout` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `waktuMulai` varchar(45) DEFAULT NULL,
-  `durasi` int(11) DEFAULT NULL,
+  `waktuMulai` time NOT NULL,
+  `durasi` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama` varchar(75) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `tryout`
+--
+
+INSERT INTO `tryout` (`id`, `waktuMulai`, `durasi`, `tanggal`, `nama`) VALUES
+(1, '23:00:00', 120, '0000-00-00', 'DingDong tryout'),
+(2, '09:38:00', 1, '2014-03-06', 'Lalala'),
+(3, '09:59:00', 120, '2014-03-27', 'Tryout lalalallalal');
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `jawaban`
+--
+ALTER TABLE `jawaban`
+  ADD CONSTRAINT `jawabaan_pengerjaan` FOREIGN KEY (`idpengerjaan`) REFERENCES `pengerjaantryout` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `soal_jawab` FOREIGN KEY (`idsoal`) REFERENCES `soal` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `konten`
@@ -183,8 +241,7 @@ ALTER TABLE `konten`
 -- Constraints for table `opsi`
 --
 ALTER TABLE `opsi`
-  ADD CONSTRAINT `pengerjaan_opsi` FOREIGN KEY (`idPengerjaan`) REFERENCES `pengerjaantryout` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `soal_opsi` FOREIGN KEY (`idTryout`, `idSoal`) REFERENCES `soal` (`idTryout`, `nomor`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `soal_opsi` FOREIGN KEY (`idsoal`) REFERENCES `soal` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `pengerjaantryout`
@@ -197,13 +254,13 @@ ALTER TABLE `pengerjaantryout`
 -- Constraints for table `profil`
 --
 ALTER TABLE `profil`
-  ADD CONSTRAINT `pengguna_id` FOREIGN KEY (`id`) REFERENCES `pengguna` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `pengguna_id` FOREIGN KEY (`idpengguna`) REFERENCES `pengguna` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `soal`
 --
 ALTER TABLE `soal`
-  ADD CONSTRAINT `tryout_id` FOREIGN KEY (`idTryout`) REFERENCES `tryout` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `tryout_soal` FOREIGN KEY (`idtryout`) REFERENCES `tryout` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
