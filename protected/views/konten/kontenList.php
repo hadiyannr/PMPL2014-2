00d3bffc1bc68244b4515dc873de5ca4e70590bc
Fariz Ikhwantri <farizikhwantri@gmail.com>
2014-04-13 17:54:15 +0700

    view konten

--- protected/views/konten/kontenList.php
+++ protected/views/konten/kontenList.php
@@ -8,8 +8,8 @@ $this->breadcrumbs=array(
 	<div class="row clearfix">
 		<div class="col-md-12 column">
 			<div class="row clearfix">
-				<div class="col-md-12 column">
-					<div class="carousel slide" id="carousel-748657">
+				<div class="col-md-7 column">
+					<div class="carousel slide" id="carousel-748657" style="width: 600px"> 
 						<ol class="carousel-indicators">
 							<li class="active" data-slide-to="0" data-target="#carousel-748657">
 							</li>
@@ -20,7 +20,7 @@ $this->breadcrumbs=array(
 						</ol>
 						<div class="carousel-inner">
 							<div class="item active">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/1">
+								<img alt="" src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/03.jpg" style="height: 400px;">
 								<div class="carousel-caption">
 									<h4>
 										First Thumbnail label
@@ -31,7 +31,7 @@ $this->breadcrumbs=array(
 								</div>
 							</div>
 							<div class="item">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/2">
+								<img alt="" src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/05.jpg" style="height: 400px;">
 								<div class="carousel-caption">
 									<h4>
 										Second Thumbnail label
@@ -42,7 +42,7 @@ $this->breadcrumbs=array(
 								</div>
 							</div>
 							<div class="item">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/3">
+								<img alt="" src="<?php echo Yii::app()->request->baseUrl; ?>/images/slider/KWG-2011.jpg" style="height: 400px;">
 								<div class="carousel-caption">
 									<h4>
 										Third Thumbnail label
@@ -54,43 +54,74 @@ $this->breadcrumbs=array(
 							</div>
 						</div> <a class="left carousel-control" href="#carousel-748657" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-748657" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 					</div>
-					<div class="row clearfix">
+				</div>
+				<div class="col-md-4 column">
+					<!-- <div class="row clearfix">
 						<div class="col-md-12 column">
-						<!-- Pembatas -->
 						</div>
 					</div>
 					<div class="row clearfix">
 						<div class="col-md-12 column">
-						<!-- Pemabatas -->
 						</div>
-					</div>
+					</div> -->
+					<?php 	$counter = 0; ?>
 					<div class="panel-group" id="panel-898425">			
 					<?php
 					foreach ($model as $konten){?>
-						<div class="col-md-12" overflow: scroll>
+						<!-- <div class="col-md-12" overflow: scroll>
 						    <?php echo CHtml::link($konten->judul,array('index','id'=>$konten->id))?>
 						    <br>
-					    </div>
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								 <a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-898425" href="#panel-element-55533"><?php echo "$konten->judul" ?></a>
+					    </div> -->
+					    <?php 
+					    	$judul = $konten->judul;
+					    if ($counter==0) {?>
+					    	<div class="panel panel-default">
+								<div class="panel-heading">
+									<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-898425" href="#panel-element-<?php echo $counter;?>"><?php echo "$konten->judul" ?></a>
+								</div>
+								<div id="panel-element-<?php echo $counter;?>" class="panel-collapse collapse">
+									<div class="panel-body">
+										<div class="col-md-12 column">
+											<h2>
+												<?php echo $konten->judul;?>
+											</h2>
+											<p align="justify">
+												<?php echo $konten->isi;?>
+											</p>
+											<p>
+												<?php echo CHtml::link("More details",array('index','id'=>$konten->id))?>
+											</p>
+										</div>
+									</div>
+								</div>
 							</div>
-							<div id="panel-element-55533" class="panel-collapse collapse">
+					    <?php
+					    	$counter++;
+					    } 
+					    else {?>
+					    	<div class="panel panel-default">
+							<div class="panel-heading">
+								 <a class="panel-title" data-toggle="collapse" data-parent="#panel-898425" href="#panel-element-<?php echo $counter;?>">Fakultas Kedokteran Gigi</a>
+							</div>
+							<div id="panel-element-<?php echo $counter;?>" class="panel-collapse collapse">
 								<div class="panel-body">
 									<div class="col-md-12 column">
 										<h2>
-											Program Studi atau Jurusan
+											<?php echo $konten->judul;?>
 										</h2>
 										<p align="justify">
-											Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+											<?php echo $konten->isi;?>
 										</p>
 										<p>
-											<?php echo CHtml::link($konten->judul,array('index','id'=>$konten->id))?>
+											<?php echo CHtml::link("More details",array('index','id'=>$konten->id))?>
 										</p>
 									</div>
 								</div>
 							</div>
 						</div>
+					    <?php 
+					    	$counter++;
+					    }?>
 					<?php }?>
 					</div>
 				</div>
