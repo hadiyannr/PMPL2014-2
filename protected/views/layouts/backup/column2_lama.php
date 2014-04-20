01038e324b42cb7fdea7b0f717c22ff03342d063
hanifnaufal <hanifnaufal7557@gmail.com>
2014-04-20 10:43:33 +0700

    Layout admin

--- protected/views/layouts/backup/column2_lama.php
+++ protected/views/layouts/backup/column2_lama.php
@@ -1,11 +1,11 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="col-md-10">
+<div class="span-19">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
 </div>
-<div class="col-md-2">
+<div class="span-5 last">
 	<div id="sidebar">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,62 +1,118 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+<!--	<link rel="stylesheet" type="text/css" href="--><?php //echo Yii::app()->request->baseUrl; ?><!--/css/screen.css" media="screen, projection" />-->
+<!--	<link rel="stylesheet" type="text/css" href="--><?php //echo Yii::app()->request->baseUrl; ?><!--/css/print.css" media="print" />-->
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-        
-        
+
+    <!--bootstrap-->
+    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/main.css">
+
+    <!--font-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <style>body{font-family: 'Open Sans', sans-serif;}</style>
+
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/jquery-1.11.0.min.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/bootstrap.min.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/main.js"></script>
         
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
 
-<div class="container" id="page">
+    <header>
+        <div class="header-nav">
+            <div class="container">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/admin/index')),
-                                array('label'=>'Konten', 'url'=>array('/adminKonten/index'), 'visible'=>Yii::app()->user->isAdmin()),
-                                array('label'=>'Tryout', 'url'=>array('/adminTryout/index'), 'visible'=>Yii::app()->user->isAdmin()),
-                                array('label'=>'Web Home', 'url'=>array('/site/index')),
-				array('label'=>'Login', 'url'=>array('/admin/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                <div class="navbar-header">
+                    <?php echo CHtml::link("Admin SiapMasukUI.com", array('site/index'),array('class'=>'navbar-brand')); ?>
+                </div>
+
+                <?php $controllerName = $this->getId();?>
+                <nav role="navigation" class="navbar-default">
+                    <?php if(Yii::app()->user->isAdmin()):?>
+                    <ul class="nav navbar-nav">
+                        <li class="<?php echo (in_array($controllerName, array("site")))?"active":""?>">
+                            <?php echo CHtml::link("Home", array('admin/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                        <li class="<?php echo (in_array($controllerName, array("site")))?"active":""?>">
+                            <?php echo CHtml::link("Konten", array('adminKonten/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                        <li class="<?php echo (in_array($controllerName, array("tryout","pengerjaanTryout")))?"active":""?>">
+                            <?php echo CHtml::link("Tryout", array('adminTryout/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                        <?php endif;?>
+                        <li>
+                            <?php echo CHtml::link("Web Home", array('site/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <?php if (Yii::app()->user->isGuest) { ?>
+                            <li>
+                                <a href="#Login" data-toggle="modal" style="color: #ecf0f1">Login</a>
+                            </li>
+                        <?php } ?>
+                        <?php if (!Yii::app()->user->isGuest) { ?>
+                            <li>
+                                <?php echo CHtml::link("Logout", array('site/logout'),array('style'=>"color: #ecf0f1")); ?>
+                            </li>
+                        <?php } ?>
+                    </ul>
+
+                    <?php $this->widget('UserLogin',array('visible'=>Yii::app()->user->isGuest)); ?>
+                </nav>
+
+            </div>
+        </div>
+    </header>
+
+    <div class="container content">
+        <?php if(isset($this->breadcrumbs)):?>
+            <?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
 		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+        <?php endif?>
 
-	<?php echo $content; ?>
+        <?php echo $content; ?>
 
-	<div class="clear"></div>
+        <div class="clear"></div>
+    </div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by SiapMasukUI.com<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
 
-</div><!-- page -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 social">
+                    <a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ico-facebook.png" alt="FB"><span>FACEBOOK</span></a>
+                    <a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ico-twitter.png" alt="Twitter"><span>TWITTER</span></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#">ABOUT</a>&nbsp;&middot;&nbsp;
+                    <a href="#">PRIVACY POLICY</a>&nbsp;&middot;&nbsp;
+                    <a href="#">CONTACT</a>&nbsp&nbsp;
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    Copyright &copy; <?php echo date('Y'); ?> by SiapMasukUI.com<br/>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 
 </body>
 </html>
