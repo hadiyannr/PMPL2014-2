4986e1f288d18511c54aa399a10c73d2608704dd
Fariz Ikhwantri <farizikhwantri@gmail.com>
2014-05-05 12:35:15 +0700

    lala

---
+++ protected/views/site/forgetPasswordForm.php
@@ -0,0 +1,7 @@
+<form role="form">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
