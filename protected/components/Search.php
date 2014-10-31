3a9eccc2d409924c2dc65f437f469da87e8a56ab
hanifnaufal <hanifnaufal7557@gmail.com>
2014-10-31 21:12:48 +0700

    fix search failure

--- protected/components/Search.php
+++ protected/components/Search.php
@@ -7,7 +7,7 @@ class Search extends Portlet
     {                
         if(isset($_POST['keyword']))
         {            
-            $this->owner->redirect(array('konten/konten/search','keyword'=>$_POST['keyword']));
+            $this->owner->redirect(array('/konten/konten/search','keyword'=>$_POST['keyword']));
         }
         
         $this->render('search');
