027d125c2f7782e29d9de9c67c8a2cba7c7c9bff
bonitabone <ramadhanibonita@yahoo.com>
2014-12-23 13:16:53 +0700

    memisahkan tiap css

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -36,136 +36,191 @@
 
                 <div class="navbar-header">
                     <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/admin/index">
-                    <img class="logo-admin" src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo-admin.png" alt="">
+                        <img class="logo-admin" src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo-admin.png" alt="">
                     </a> 
-                </div>
 
-                <?php $controllerName = $this->getId();?>
-                <nav role="navigation" class="navbar-default">
-                    <ul class="nav navbar-nav">
-                        <?php if(Yii::app()->user->isAdmin()):?>
-                            <li class="<?php echo (in_array($controllerName, array("adminContent")))?"active":""?>">
-                                <?php echo CHtml::link("Konten", array('/konten/adminContent/index'),array('style'=>"color: #ecf0f1")); ?>
-                            </li>
-                            <li class="<?php echo (in_array($controllerName, array("adminTryout")))?"active":""?>">
-                                <?php echo CHtml::link("Tryout", array('/tryout/adminTryout/index'),array('style'=>"color: #ecf0f1")); ?>
-                            </li>
-                            <li class="<?php echo (in_array($controllerName, array("kategori")))?"active":""?>">
-                                <?php echo CHtml::link("Kategori", array('/konten/kategori/index'),array('style'=>"color: #ecf0f1")); ?>
-                            </li>
-                        <?php endif;?>
-                        <li>
-                            <?php echo CHtml::link("Web Home", array('/site/index'),array('style'=>"color: #ecf0f1")); ?>
-                        </li>
+                    <div class="dropdown responsive-nav">
+                      <a id="dLabel" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">
+                        <hr><hr><hr>
+                    </a>
+                    <?php $controllerName = $this->getId();?>
+                    <ul class="dropdown-menu responsive-dropdown" role="menu" aria-labelledby="dLabel">
+                       <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color: #ecf0f1">Konten <b class="caret"></b></a>
+                        <ul>
+                            <?php
+                            $kategoris = Kategori::model()->findAll();
+                            foreach ($kategoris as $kategori): ?>
+                            <li> <?php echo CHtml::link($kategori->nama, array('/konten/konten/kategori','idcategory'=>$kategori->id),array('style'=>"color: #ecf0f1"));?></li>
+                        <?php endforeach;?>
                     </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php if (Yii::app()->user->isGuest) { ?>
+                </li>
+                <li class="<?php echo (in_array($controllerName, array("tryout","pengerjaanTryout")))?"active":""?>">
+                    <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/tryout/tryout/index">
+                        <img class="icon" src="<?php echo Yii::app()->request->baseUrl; ?>/images/iconTO.png" alt="">
+                        <span>Tryout</span>
+                    </a>
+                </li>
+                <li class="<?php echo (in_array($controllerName, array("forum","Thread","User","Post")))?"active":""?>">
+                    <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/forum">
+                        <img class="icon" src="<?php echo Yii::app()->request->baseUrl; ?>/images/forum.png" alt="">
+                        <span>Forum</span>
+                    </a>
+                </li>
+
+                <?php if(Yii::app()->user->isAdmin()):?>
+                    <li>
+                        <?php echo CHtml::link("Admin", array('/admin/index'),array('style'=>"color: #ecf0f1")); ?>
+                    </li>
+                <?php endif;?>
+
+                <?php if (Yii::app()->user->isGuest) { ?>
+                    <li>
+                        <a href="#Login" data-toggle="modal" style="color: #ecf0f1">Login</a>
+                    </li>      
+                    <li>
+                        <a href="#SignUp" data-toggle="modal" style="color: #ecf0f1">Daftar</a>
+                    </li>                   
+                    <?php } ?>
+                    <?php if (!Yii::app()->user->isGuest) { ?>
+                        <li class="<?php echo (in_array($controllerName, array("profil")))?"active":""?>">
+                            <?php echo CHtml::link("Profil", array('/profil/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>      
+                        <li>
+                            <?php echo CHtml::link("Logout", array('/site/logout'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>                   
+                        <?php } ?>
+
+                    </ul>
+                </div>
+            </div>
+
+            <?php $controllerName = $this->getId();?>
+            <nav role="navigation" class="navbar-default">
+                <ul class="nav navbar-nav">
+                    <?php if(Yii::app()->user->isAdmin()):?>
+                        <li class="<?php echo (in_array($controllerName, array("adminKonten")))?"active":""?>">
+                            <?php echo CHtml::link("Konten", array('/konten/adminKonten/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                        <li class="<?php echo (in_array($controllerName, array("adminTryout")))?"active":""?>">
+                            <?php echo CHtml::link("Tryout", array('/tryout/adminTryout/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                        <li class="<?php echo (in_array($controllerName, array("kategori")))?"active":""?>">
+                            <?php echo CHtml::link("Kategori", array('/konten/kategori/index'),array('style'=>"color: #ecf0f1")); ?>
+                        </li>
+                    <?php endif;?>
+                    <li>
+                        <?php echo CHtml::link("Web Home", array('/site/index'),array('style'=>"color: #ecf0f1")); ?>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if (Yii::app()->user->isGuest) { ?>
+                        <li>
+                            <a href="#Login" data-toggle="modal" style="color: #ecf0f1">Login</a>
+                        </li>
+                        <?php } ?>
+                        <?php if (!Yii::app()->user->isGuest) { ?>
                             <li>
-                                <a href="#Login" data-toggle="modal" style="color: #ecf0f1">Masuk</a>
+                                <?php echo CHtml::link("Logout", array('/site/logout'),array('style'=>"color: #ecf0f1")); ?>
                             </li>
                             <?php } ?>
-                            <?php if (!Yii::app()->user->isGuest) { ?>
-                                <li>
-                                    <?php echo CHtml::link("Logout", array('/site/logout'),array('style'=>"color: #ecf0f1")); ?>
-                                </li>
-                                <?php } ?>
-                            </ul>
+                        </ul>
 
-                            <?php $this->widget('UserLogin',array('visible'=>Yii::app()->user->isGuest)); ?>
-                        </nav>
-                    </div>
+                        <?php $this->widget('UserLogin',array('visible'=>Yii::app()->user->isGuest)); ?>
+                    </nav>
                 </div>
-            </header>
+            </div>
+        </header>
 
-            <div class="container content admin-content">
-                <?php if(isset($this->breadcrumbs)):?>
-                    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
-                       'links'=>$this->breadcrumbs,
-                       )); ?><!-- breadcrumbs -->
-                   <?php endif?>
+        <div class="container content admin-content">
+            <?php if(isset($this->breadcrumbs)):?>
+                <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                 'links'=>$this->breadcrumbs,
+                 )); ?><!-- breadcrumbs -->
+             <?php endif?>
 
-                   <?php echo $content; ?>
+             <?php echo $content; ?>
 
-                   <div class="clear"></div>
-               </div>
+             <div class="clear"></div>
+         </div>
 
 
 
-               <footer>
-                <div class="container-fluid">
-                    <div class="col-md-3">
-                        <p><span class="ft-golden-yellow">SiapMasukUI.com</span> adalah website yang bertujuan mempersiapkan siswa SMA untuk lebih matang dalam menghadapi ujian saringan masuk UI. SiapMasukUi.com memberkan variasi soal TO beserta simulasi dan jawaban TO. Pengguna juga dapat melihat hasil statistik test mereka pada halaman profile pengguna.</p>
-                        <p>Fitur forum juga penting untu berdiskusi antar pengguna. Selain itu, terdapat portal berita yang disinkronisasikan dari humas.ui.ac.id.<br>Selamat bergabung!</p>
-                        <p><span class="ft-golden-yellow">WE ARE THE YELLOW JACKET!</span></p>
+         <footer>
+            <div class="container-fluid">
+                <div class="col-md-3">
+                    <p><span class="ft-golden-yellow">SiapMasukUI.com</span> adalah website yang bertujuan mempersiapkan siswa SMA untuk lebih matang dalam menghadapi ujian saringan masuk UI. SiapMasukUi.com memberkan variasi soal TO beserta simulasi dan jawaban TO. Pengguna juga dapat melihat hasil statistik test mereka pada halaman profile pengguna.</p>
+                    <p>Fitur forum juga penting untu berdiskusi antar pengguna. Selain itu, terdapat portal berita yang disinkronisasikan dari humas.ui.ac.id.<br>Selamat bergabung!</p>
+                    <p><span class="ft-golden-yellow">WE ARE THE YELLOW JACKET!</span></p>
 
-                    </div>
-                    <div class="col-md-2 footer-mid">
-                        <p>&copy;</p>
-                        <p>siapmasukUI.com</p>
-                        <p>2014</p>
-                    </div>
-                    <div class="col-md-7">
-                        <div class="col-maps">
-                            <div class="col1-maps">
-                                <h5>Jelajahi kampus impian kamu disini!</h5>
-                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/iconGPS.png" alt="">
-                            </div>
-                            <div class="col2-maps">
-                                <div id="map" style="width: 100%; height:100%"></div>
-                            </div>
+                </div>
+                <div class="col-md-2 footer-mid">
+                    <p>&copy;</p>
+                    <p>siapmasukUI.com</p>
+                    <p>2014</p>
+                </div>
+                <div class="col-md-7">
+                    <div class="col-maps">
+                        <div class="col1-maps">
+                            <h5>Jelajahi kampus impian kamu disini!</h5>
+                            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/iconGPS.png" alt="">
+                        </div>
+                        <div class="col2-maps">
+                            <div id="map" style="width: 100%; height:100%"></div>
                         </div>
                     </div>
                 </div>
-            </footer>
+            </div>
+        </footer>
 
-            <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-            <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-            <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/gmaps.js"></script>
-            <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/jquery-1.11.0.min.js"></script>
-            <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/bootstrap.min.js"></script>
-            <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/main.js"></script>
-            <script>
-                var Site = {
-                    init: function(){
-                        $('html').removeClass('no-js');
-                        if($('#zoom').length)
-                            Site.zoom();
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/gmaps.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/jquery-1.11.0.min.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/bootstrap.min.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/main.js"></script>
+        <script>
+            var Site = {
+                init: function(){
+                    $('html').removeClass('no-js');
+                    if($('#zoom').length)
+                        Site.zoom();
 
-                        if($('#map').length)
-                            Site.map();
-                    },
+                    if($('#map').length)
+                        Site.map();
+                },
 
-                    zoom: function() {
-                        $('#zoom').elevateZoom({
-                            zoomWindowWidth: 300,
-                            zoomWindowHeight: 300,
-                            easing:true
-                        });
-                    },
+                zoom: function() {
+                    $('#zoom').elevateZoom({
+                        zoomWindowWidth: 300,
+                        zoomWindowHeight: 300,
+                        easing:true
+                    });
+                },
 
-                    map: function() {
-                        var mark = new GMaps({
-                            div:"#map",
-                            lat: -6.368074,
-                            lng: 106.829559
+                map: function() {
+                    var mark = new GMaps({
+                        div:"#map",
+                        lat: -6.368074,
+                        lng: 106.829559
 
-                        })
-                        mark.addMarker({
-                            lat: -6.368074,
-                            lng: 106.829559,
-                            title: 'Universitas Indonesia',
-                            click: function(e) {
-                                alert('You clicked in this marker');
-                            }
-                        });
-                    },  
-                };
+                    })
+                    mark.addMarker({
+                        lat: -6.368074,
+                        lng: 106.829559,
+                        title: 'Universitas Indonesia',
+                        click: function(e) {
+                            alert('You clicked in this marker');
+                        }
+                    });
+                },  
+            };
 
-                $(function (){
-                    Site.init();
-                });
+            $(function (){
+                Site.init();
+            });
 
-            </script>
+        </script>
 
-        </body>
-        </html>
+    </body>
+    </html>
