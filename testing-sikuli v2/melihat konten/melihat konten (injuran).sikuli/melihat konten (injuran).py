d53855ce82b1efd4e6c93043fbe2419ab15faee5
Fariz Ikhwantri <farizikhwantri@gmail.com>
2014-12-25 16:04:03 +0700

    organize folder

--- testing-sikuli v2/melihat konten/melihat konten (injuran).sikuli/melihat konten (injuran).py
+++
@@ -1,7 +0,0 @@
-find("Konten.png")
-click("Konten.png")
-click("lnformasiFak.png")
-wait("HomeDaftarKo.png")
-find("SejarahFakul.png")
-click(Pattern("SejarahFakul.png").targetOffset(96,37))
-assert exists("FakultasTekn.png")
--- testing-sikuli v2/melihat profil/melihat profil.sikuli/melihat profil.py
+++
@@ -1,4 +0,0 @@
-find("Profil.png")
-click("Profil.png")
-assert exists("HanifNaufalJ.png")
-assert exists("UbahProfilRi.png")
