ed7970fc8c1270f2457792a0eab73b2a698c0e8d
bonitabone <ramadhanibonita@yahoo.com>
2014-12-27 21:36:56 +0700

    benerin button dan halaman Riwayat Uji Coba

--- protected/modules/tryout/views/answerSheet/historyList.php
+++ protected/modules/tryout/views/answerSheet/historyList.php
@@ -1,12 +1,13 @@
+
 <?php
-
-/* @var $this AnswerSheetController */
-/* @var $tryoutModelList Tryouts */
-/* @var $tryout Tryouts */
-
-
+$this->breadcrumbs = array(
+    'Uji Coba Kemampuan'=>array("index"),
+    'Riwayat Uji Coba',
+    );
+echo '<br>';
 ?>
-<h2 class="text-center">Riwayat Tryout</h2>
+
+<h2 class="text-center">Riwayat Uji Coba</h2>
 <br><br>
 <?php if (sizeof($tryoutModelList)): ?>
         <table class="table">
